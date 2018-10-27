958c654a135c627b8d84d502b65347cf60774154
a.shimomura <akitaka.shimomura@gmail.com>
2018-10-27 14:02:46 +0900

    jq

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery/dist/jquery.min.js
 //= require bootstrap/dist/js/bootstrap.min.js
 //= require admin-lte/dist/js/adminlte.min.js
