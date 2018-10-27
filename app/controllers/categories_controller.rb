486ff6339c6fec7903c8bb377e32e36a9df8bb9c
a.shimomura <akitaka.shimomura@gmail.com>
2018-10-27 12:49:10 +0900

    削除機能仮作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -39,13 +39,13 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
   def destroy
-    @category.destroy
+    category = Category.find!(params[:id])
+    rediret_to categories_url, notice: 'このカテゴリーは使用されているため削除できません' if category.outlay
+
+    category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to categories_url, notice: 'カテゴリーを削除しました'}
     end
   end
 
